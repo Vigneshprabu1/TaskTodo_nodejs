2c620256f60c9e5a6a436326fb3db9432dbc382a
VigneshprabuSampathkumar <vigneshprabu479@gmail.com>
2020-06-27 18:31:05 +0530

    Vp commit mlab link

--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/taskslist',{useNewUrlParser: true,useUnifiedTopology:true});
+
+// mongoose.connect('mongodb://localhost:27017/taskslist',{useNewUrlParser: true,useUnifiedTopology:true});
 
 
 /** Production DataBase */
-// mongoose.connect('mongodb+srv://trcbackoffice:trcbackoffice123@clustertrc-x0fss.mongodb.net/trc?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect('mongodb://admin:Admin12345678@ds119323.mlab.com:19323/tasktodolist',{useNewUrlParser: true,useUnifiedTopology: true});
 
-/** Testing DataBase */
-// mongoose.connect('mongodb+srv://farm2bag:shine1234@cluster0-hhq55.mongodb.net/trc?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true});
 mongoose.set('useFindAndModify',true);
 
 var db= mongoose.connection;
